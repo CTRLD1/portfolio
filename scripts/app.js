090c5ef7c7ed6a17e6f983b05a81a016311166f2
dansubaie2 <dansubaie029@gmail.com>
2025-09-12 10:09:26 +0300

    update: added 2 buttons for projects & made them interactive by js

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,29 @@
-// im currently working on the structure and design, 
-// i plan to start with javascript once im 100% sure with the design so i dont get confused
+function init() {
+    // all of our code goes here
+
+
+    // 1 cache our element
+    const buttons = document.querySelectorAll('.project-Bttn')
+
+
+    // 3 write a function to run when the event happens
+
+
+
+
+    // 2 add an event listener
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const link = button.getAttribute('data-link');
+            window.open(link, '_blank');
+        })
+    })
+
+}
+
+document.addEventListener('DOMContentLoaded', init)
+
+
+
+
+
